76505ae505f88bcf2d165f9661d4b3207cbdf2ec
Robert Norris <rob@cataclysm.cx>
2008-10-13 21:51:38 +1100

    get rid of all the beos stuff and regen

--- src/clamp_includes/Plugin_ConstructorStruct.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLAMP_PLUGIN_CONSTRSTRUCT_H
-#define CLAMP_PLUGIN_CONSTRSTRUCT_H
-
-struct PluginConstructorStruct {
-	int Version;	//	Filled in directly by the NewPlugin function
-	const char *SettingsFolder;
-};
-
-#endif
